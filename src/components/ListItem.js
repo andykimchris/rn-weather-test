cc56cc79366f8db1a491a480d54cb27192a2a50a
andykimchris <andrew.muchiri97@gmail.com>
2023-08-28 19:39:40 +0300

    feat: creates funvtional components to render lists and rows

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,21 +1,46 @@
 import { Feather } from '@expo/vector-icons'
+import moment from 'moment'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { weatherType } from '../utilities/weatherType'
 
 const ListItem = ({ dt_txt, min, max, condition }) => {
+  const { item, date, temp, dateTextWrapper } = styles
+
   return (
-    <View style={styles.item}>
-      <Feather name={'sun'} size={50} color={'white'} />
-      <Text style={styles.date}>{dt_txt}</Text>
-      <Text style={styles.temp}>{min}</Text>
-      <Text style={styles.temp}>{max}</Text>
+    <View style={item}>
+      <Feather name={weatherType[condition]?.icon} size={50} color={'white'} />
+      <View style={dateTextWrapper}>
+        <Text style={date}>{moment(dt_txt).format('dddd')}</Text>
+        <Text style={date}>{moment(dt_txt).format('h:mm:ss a')}</Text>
+      </View>
+      <Text style={temp}>{`${Math.round(min)}° / ${Math.round(max)}°`}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: 'royalblue' },
-  text: { color: 'black' },
-  image: { flex: 1 }
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderWidth: 5,
+    backgroundColor: 'indianred'
+  },
+  temp: {
+    color: 'white',
+    fontSize: 20
+  },
+  date: {
+    color: 'white',
+    fontSize: 15
+  },
+  dateTextWrapper: {
+    flexDirection: 'column'
+  }
 })
 
 export default ListItem
