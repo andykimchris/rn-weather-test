abea855f0b3ec6c48f2fe167c58807e9beae1c9c
andykimchris <andrew.muchiri97@gmail.com>
2023-08-28 19:45:38 +0300

    chore: creates hook to request user location and weather data

---
+++ src/hooks/useGetWeather.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-extra-semi */
+import { WEATHER_API_KEY } from '@env'
+import * as Location from 'expo-location'
+import { useEffect, useState } from 'react'
+
+export const useGetWeather = () => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [weather, setWeather] = useState([])
+  const [lat, setLat] = useState(null)
+  const [lon, setLon] = useState(null)
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      )
+      const data = await res.json()
+      setWeather(data)
+    } catch (err) {
+      setError('could not fetch weather')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+
+      if (status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLat(location.coords.latitude)
+      setLon(location.coords.longitude)
+      await fetchWeatherData()
+    })()
+  }, [lat, lon])
+
+  return [loading, error, weather]
+}
